62c1f4e3817c3e92cca069b90ac3f348eb6de80a
Simon Laustsen <sl@mapspeople.com>
2019-11-04 08:33:37 +0100

    Listeners and bindings added for directive

--- dragNdrop/src/app/directives/drag-ndrop.directive.ts
+++ dragNdrop/src/app/directives/drag-ndrop.directive.ts
@@ -1,10 +1,40 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, HostBinding, EventEmitter, Output } from '@angular/core';
 
 @Directive({
-  selector: '[dragNdrop]'
+	selector: '[dragNdrop]'
 })
 export class DragNdropDirective {
+	@Output() private filesChangeEmiter : EventEmitter<FileList> = new EventEmitter();
+	@HostBinding('style.background') private background = '#eee';
 
-  constructor() { }
+	constructor() { }
+
+	@HostListener('dragover', ['$event']) onDragOver(event) {
+		console.log(event);
+		event.preventDefault();
+		event.stopPropagation();
+		this.background = '#999';
+		let files = event.dataTransfer.files;
+		if (files.length > 0) {
+			//do some stuff here
+		}
+	}
+
+	@HostListener('dragleave', ['$event']) public onDragLeave(event) {
+		event.preventDefault();
+		event.stopPropagation();
+		this.background = '#eee'
+		//do some stuff
+	}
+
+	@HostListener('drop', ['$event']) public onDrop(event) {
+		event.preventDefault();
+		event.stopPropagation();
+		let files = event.dataTransfer.files;
+		if (files.length > 0) {
+			this.background = '#eee'
+			this.filesChangeEmiter.emit(files);
+		}
+	}
 
 }
